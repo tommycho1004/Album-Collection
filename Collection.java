e80729bb56ceb9cef5951686fe29101a0cc37f65
Dong Gun (Tommy) Cho <tommycho1004@gmail.com>
2021-09-21 18:34:39 -0400

    Update Collection.java

--- Collection.java
+++ Collection.java
@@ -10,6 +10,12 @@ import java.util.StringTokenizer;
 public class Collection {
     private Album[] albums;
     private int numAlbums; //number of albums currently in the collection
+    
+    /**
+     * Finds the index of a certain album within the collection.
+     * @param album
+     * @return the index of the target album.
+     */
     private int find(Album album) { //find the album index, or return -1 if no match
         for (var i = 0; i < albums.length; i++) {
             if (albums[i].equals(album)) {
@@ -18,6 +24,11 @@ public class Collection {
         }
         return -1;
     }
+    
+    /**
+     * Increases the size of the album list by 4.
+     * Use whenever the album list fills up to max capacity so we never have a completely full list.
+     */
     private void grow() {
         Album newList[] = new Album[numAlbums+3];
         for (int i = 0; i < numAlbums; i++)
@@ -26,6 +37,12 @@ public class Collection {
         }
         albums = newList;
     }//increase the capacity of the array list by 4
+    
+    /**
+     * Adds an album to the collection.
+     * @param album
+     * @return true if the album was successfully added and false otherwise.
+     */
     public boolean add(Album album) {
         if (album.getDate.isValid == true && find(album) == -1) {
             numAlbums++; // add to numAlbums
@@ -37,26 +54,54 @@ public class Collection {
         }
         return false;
     }
+    
+    /**
+     * Removes an album from the collection.
+     * @param album
+     * @return true if the album was successfully removed and false otherwise.
+     */
     public boolean remove(Album album) {
 
     }
+    
+    /**
+     * Sets the availability of an album to unavailable.
+     * @param album
+     * @return true if the album was lent and false otherwise.
+     */
     public boolean lendingOut(Album album) {
         album.setNotAvailable();
     } //set to not available
+    
+    /**
+     * Sets the availability of an album to available.
+     * @param album
+     * @return true if the album was returned and false otherwise.
+     */
     public boolean returnAlbum(Album album) {
 
     } //set to available
+    
+    /**
+     * Prints the collection of albums in no specific order.
+     */
     public void print() {
 
     } //display the list without specifying the order
+    
+    /**
+     * Prints the collection of albums in order of release date.
+     */
     public void printByReleaseDate() {
 
     }
+    
+    /**
+     * Prints the collection of albums in order of genre.
+     */
     public void printByGenre() {
 
     }
-
-
 }
 
 
