7a589c8822fe7d2ff98f8dd7cb032b21a125613e
Dong Gun (Tommy) Cho <tommycho1004@gmail.com>
2021-09-24 16:58:19 -0400

    Update Genre.java

--- Genre.java
+++ Genre.java
@@ -1,8 +1,24 @@
-package albums;
-
 /**This enum class defines the genres of the album collection
  *
  */
 public enum Genre{
     Pop, Country, Classical, Jazz, Unknown;
+    public String genreString()
+    {
+        if (this == Pop){
+            return "Pop";
+        }
+        else if(this == Country){
+            return "Country";
+        }
+        else if(this == Classical){
+            return "Classical";
+        }
+        else if(this == Jazz){
+            return "Jazz";
+        }
+        else{
+            return "Unknown";
+        }
+    }
 }
